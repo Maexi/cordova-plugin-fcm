4b199a91077a7e180787a51241f681ae786b869c
Max Ruff <max.ruff@macio.de>
2016-10-20 17:10:29 +0200

    fixed BroadCastReceiver inports

--- src/android/FCMPluginDismissBroadcastReceiver.java
+++ src/android/FCMPluginDismissBroadcastReceiver.java
@@ -1,18 +1,9 @@
 package com.gae.scaffolder.plugin;
 
-import android.app.Activity;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.v4.content.LocalBroadcastManager;
+import android.content.BroadcastReceiver ;
 import android.util.Log;
-
-import java.util.Map;
-import java.util.HashMap;
 
 public class FCMPluginDismissBroadcastReceiver extends BroadcastReceiver {
     private static String TAG = "FCMPlugin";
